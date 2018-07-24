4d52b2aa8b948522f8e77a78c5171446c8826131
Arrez <arrez101@gmail.com>
2018-07-24 21:51:10 +0800

    bouncey test

--- Example1.js
+++ Example1.js
@@ -4,18 +4,28 @@ class Example1 extends Phaser.Scene{
       }
 
       preload(){
-          this.load.image('hoxy','Assets/hoxy.gif');
+        this.load.image('bg','Assets/background.jpg');
+        this.load.image('haram','Assets/haram.png');
+
       }
 
       create(){
-          this.image = this.add.image(400,300,'hoxy');
+        this.background = this.add.image(400,300,'bg')
+        var haram = this.physics.add.image(600,500,'haram');
 
         this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+
+        haram.setVelocity(200, 300);
+        haram.setBounce(1, 1);
+        haram.setCollideWorldBounds(true);
+
         }
-        update(delta){
+
+
+/*         update(delta){
             if(this.key_A.isDown){
                 this.image.x--;
         }
@@ -28,7 +38,7 @@ class Example1 extends Phaser.Scene{
             if(this.key_W.isDown){
                 this.image.y--;
         }
-      } 
+      }  */
 
 
 
